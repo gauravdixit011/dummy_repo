7b1dd75108314b36bd92faa7feb8fb807c68e2c3
Gaurav Dixit <dgaurav300@gmail.com>
2026-09-14 12:01:00 +0530

    Initial Commit

---
+++ example1.py
@@ -0,0 +1,13 @@
+a=5
+b=6
+c = a+b
+print(c)
+
+# Working Directory
+# git init
+# git add example1.py
+# git commit -m "Initial commit"
+# git remote add origin <remote_repository_URL>
+# git push -u origin master
+# # To check the status of the repository
+# git status
